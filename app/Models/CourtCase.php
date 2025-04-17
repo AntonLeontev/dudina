11a3ecf9315cf78cbd4ebec04f160bd970806210
DoczzzMega <roma.gunko87@gmail.com>
2025-04-17 18:30:29 +0500

    create CourtCase model with migration

---
+++ app/Models/CourtCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CourtCase extends Model
+{
+    protected $fillable = [
+
+        'position',
+        'path',
+    ];
+    protected static function booted(): void
+    {
+        static::creating(function (User $user): void {
+
+            $lastPosition = self::max('position');
+            $user->position = $lastPosition ? $lastPosition + 1 : 1;
+        });
+    }
+}
