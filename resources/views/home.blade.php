6579280e7a4af8c02cad14c751b8a43adcea099d
Anton <aner-anton@yandex.ru>
2025-04-20 19:41:58 +0300

    fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -758,7 +758,7 @@
                 </h2>
 
                 <iframe class="bankruptcy-video__content"
-                    src="https://vk.com/video_ext.php?oid=716842214&id=456239334&hd=2&autoplay=1" width="853"
+                    src="https://vk.com/video_ext.php?oid=716842214&id=456239334&hd=2&autoplay=0" width="853"
                     height="480" allow="autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
                     frameborder="0" allowfullscreen></iframe>
             </div>
